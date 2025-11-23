51785a02f812731f5cf5c8a1e5d19bdfec034ee1
cpint0 <cpuc000@gmail.com>
2025-11-23 16:33:12 -0300

    feat(actividades): implementar 3 modos de creación y generación automática de informes

--- Backend/dist/database/entities/empleado-cargo.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cargo } from './cargo.entity';
-import { Usuario } from './usuario.entity';
-export declare class EmpleadoCargo {
-    id: number;
-    fecha_inicio: Date;
-    fecha_fin: Date;
-    usuario: Usuario;
-    cargo: Cargo;
-}
